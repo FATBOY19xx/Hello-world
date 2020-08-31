32d70f3cf6b78b42ecf5d4498bbcbb602d5524ef
Ian Mollick <22icmollick@macon.k12.mo.us>
2020-08-31 12:12:38 -0500

    Commit1

---
+++ HelloWolrd1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWolrd1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
