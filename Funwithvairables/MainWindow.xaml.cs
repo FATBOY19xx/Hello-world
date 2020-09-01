2d3917345d8e5b4fe2f634a07b47583a1a8675a3
Ian Mollick <70287689+FATBOY19xx@users.noreply.github.com>
2020-09-01 13:44:09 -0500

    Add files via upload

---
+++ Funwithvairables/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Funwithvairables
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMyMessage_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName;
+
+            string lastName;
+
+            firstName = textBoxFirstName.Text;
+
+            lastName = textBoxLastName.Text;
+
+            labelMyMessage.Content = "Hello " + firstName + " " + lastName + ", good to see you! ";
+
+
+        }
+
+        private void buttonClearMe_Click(object sender, RoutedEventArgs e)
+        {
+
+            textBoxFirstName.Text = "";
+            
+            textBoxLastName.Text = "";
+
+            labelMyMessage.Content = "";
+
+
+
+
+
+
+        }
+    }
+}
